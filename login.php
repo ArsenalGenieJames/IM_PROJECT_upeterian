649b89b60e69834a7e1fcfaa984a6c7ae9e28ebc
geniearsenal <geniejamesarsenal.202300349@gmail.com>
2024-11-23 00:14:27 +0800

    first commit

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include('db.php'); 
+include('db.php');
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST['email'];
@@ -18,7 +18,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         if (password_verify($password, $user['password'])) {
             // Password is correct, set session  
             $_SESSION['user_id'] = $user['user_id'];
-            header("Location: home.php"); // Redirect to your home page  
+            header("Location: home.php"); 
             exit();
         } else {
             echo "Invalid password.";
@@ -45,53 +45,38 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.0/font/bootstrap-icons.css">
     <link rel="stylesheet" href="style.css">
 </head>
-<style>
-    body {
-        overflow: none;
-    }
-</style>
 
 <body>
     <div class="container">
-        <div class="row justify-content-center align-items-center">
+        <div class="row justify-content-center align-items-center vh-100 ">
+            
+            <div class="col-md-6 col-lg-4 logcon mt-5">
+                <div class="col-12 d-flex justify-content-center">
+                    <img src="secondary_lgo.png" class="img-fluid w-50 h-50" alt="secondary logo">
+                </div>
 
-            <div class="col-md-6 logcon md-5">
-
-                <img src="assets/secondary logo.png" alt="secondary logo" class="img-fluid mx-auto d-block"
-                    style="height: 200px; width: 200px;">
-                <div class="card">
-
-                    <div class="card-body text-center logcon1 mb-5 ">
-                        <form action="login.php" method="post"> 
+                <div class="card mt-5 ">
+                    <div class="card-body text-center p-5  ">
+                        <form action="login.php" method="post" class="mt-3 p-2 mb-5">
                             <div class="mb-3 text-start">
-                                <label for="email" class="form-label">Email address</label>
-                                <input type="email" name="email" class="form-control" id="email"
-                                    aria-describedby="emailHelp" placeholder="Email" required>
+                                <input type="email" name="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Email" required>
                             </div>
-
                             <div class="mb-3 text-start">
-                                <label for="password" class="form-label">Password</label>
-                                <input type="password" name="password" class="form-control" id="password"
-                                    placeholder="Password" required>
+                                <input type="password" name="password" class="form-control" id="password" placeholder="Password" required>
                             </div>
-
                             <div class="mb-3 form-check text-start">
                                 <input type="checkbox" class="form-check-input" id="rememberMe">
                                 <label class="form-check-label" for="rememberMe">Remember me</label>
                             </div>
-
-                            <button type="submit" class="btn login-set">Log In</button>
-
+                            <button type="submit" class="btn w-100 login-set">Log In</button>
                             <div class="text-center mt-3">
-                                <p>Don't have an account?
-                                    <a href="register.php" style="text-decoration: none;">Create Account</a>
-                                </p>
+                                <p>Don't have an account? <a href="register.php" class="text-decoration-none">Create Account</a></p>
                             </div>
-
                         </form>
                     </div>
                 </div>
             </div>
+
         </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
